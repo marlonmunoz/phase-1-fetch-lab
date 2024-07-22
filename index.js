4e945765f7f07ae1e47f6c3f5de72f9c40545ac0
Marlon Munoz <marlon@marlonshade.com>
2024-07-22 08:17:17 -0400

    fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json()) 
+  .then(json => renderBooks(json)) 
   
 }
+// fetch("https://anapioficeandfire.com/api/books")
+// .then((resp) => resp.json())
+// .then((json) => console.log(json));
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
